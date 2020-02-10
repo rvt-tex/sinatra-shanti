05f65589c7681ddab624e64aa1d3b9102983bbc9
github username <github email address>
2020-02-10 11:04:27 -0500

    update application controller, required securerandom, sinatra/flash, enable sessions, set sessions secret & register sinatra flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 require './config/environment'
+require 'securerandom'
+require 'sinatra/flash'
+
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "shanti-shanti-shanti-sinatra-secure"
+    register Sinatra::Flash
   end
 
   get "/" do
