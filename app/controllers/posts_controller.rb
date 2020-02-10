718dc2bee4605d6bdbaef4e3f1e250a79a8fce57
github username <github email address>
2020-02-10 15:04:28 -0500

    create the posts route for the newly created post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,4 +17,22 @@ class PostsController < ApplicationController
           redirect to "/login"
         end
     end
+
+    post '/posts' do
+        if is_logged_in?
+          post = Post.new(params)
+          post.user = current_user
+          if post.save
+            flash[:message] = "Congrats! Love Your Thought"
+            redirect to "/posts"
+          else
+            flash[:message] = "Please share your thoughts, we would love to hear them."
+            redirect to "/posts/new"
+          end
+        else
+          flash[:message] = "You need to be logged in first to access this page."
+          redirect to "/login"
+        end
+    end
+
 end 
