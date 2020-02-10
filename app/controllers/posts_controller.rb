a3dbeef0f859444fbfb8770ec51c97711480c675
github username <github email address>
2020-02-10 14:45:50 -0500

    create post route to get all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,11 @@
 class PostsController < ApplicationController
 
+    get '/posts' do
+        if is_logged_in?
+          erb :'/posts/index'
+        else
+          flash[:message] = "You need to be logged in first to access this page."
+          redirect to '/login'
+        end
+    end
 end 
