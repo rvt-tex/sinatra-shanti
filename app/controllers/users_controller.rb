5922fda359dbc0c50058e3b0d84519ee589fc9be
github username <github email address>
2020-02-10 14:10:57 -0500

    create the signup route to get the form for a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if !is_logged_in?
+          erb :'/users/new'
+        else
+          redirect to "/posts"
+        end
+    end
+
 end 
