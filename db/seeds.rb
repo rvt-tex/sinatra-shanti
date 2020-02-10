3e08579efb6bfd3617e7c12bcdf72f1ac8ce097b
github username <github email address>
2020-02-10 10:07:49 -0500

    created a seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+User.create(username: "Johnny", email: "johnny@shanti.com", password: "johnny2020", title: "Be Caring", Content: "Thousands of candles can be lit from a single candle, and the life of the candle will not be shortened.
+    Happiness never decreases by being shared. Share the love, share the joy, share the knowledge.")
+
+User.create(username: "Aakash", email: "Aakash@shanti.com", password: "Aakash2020", title: "Silent Rivers", content: "There is a saying, “If you are silent enough, you will hear the flow of the universe.
+    You will feel its rhythm. Go with this flow. Happiness lies ahead. Meditation is key.” while on another note, If you light a lamp for somebody,
+    it will also brighten your path. So please never be afraid to share your knowledge.")
+
+User.create(username: "Rahul", email: "Rahul@shanti.com", password: "rahul2020", title: "Become Tenacious", content: "What you think, you become. What you feel, you attract. What you imagine, you create.
+    Always meditate on your life and progress.
+    'Meditation brings wisdom; lack of meditation leaves ignorance. Know well what leads you forward and what holds you back, and choose the path that leads to wisdom'.")
+
+
+User.create(username: "Elani", email: "Elani@shanti.com", password: "elani27", title: "Living in the Moment" , content: "The secret of health for both mind and body is not to mourn for the past, worry about the future,
+    or anticipate troubles, but to live in the present moment wisely and earnestly.")
+
+
+User.create(username: "Niya", email: "niya18@shanti.com", password: "niya18", title: "Be the Peace" , content: "Sometimes you can find peace of mind by transferring yourself to different situations.
+    They're just reminders to stay ... calm. You should feel beautiful and you should feel safe. What you surround yourself with should bring you peace of mind and peace of spirit.")
