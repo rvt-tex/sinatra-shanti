2d01571f017befb13ed74658092e037a53e6953d
github username <github email address>
2020-02-10 10:29:22 -0500

    created post model and association

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base 
+
+    belongs_to :user
+    validates :title, presence: true, length: {minimum: 5}
+    validates :content, presence: true, length: {minimum: 10}
+end 
