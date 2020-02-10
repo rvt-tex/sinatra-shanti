0032f77828a49b3424bcb463ab336d51ddb3e0bc
github username <github email address>
2020-02-10 14:54:17 -0500

    create route for posts/new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,13 @@ class PostsController < ApplicationController
           redirect to '/login'
         end
     end
+
+    get '/posts/new' do
+        if is_logged_in?
+          erb :'/posts/new'
+        else
+          flash[:message] = "You need to be logged in first to access this page."
+          redirect to "/login"
+        end
+    end
 end 
